76a737b00031751529c4fedd6945260c099744c7
Vitalii <domovik0712@gmail.com>
2025-10-21 14:50:55 +0300

    feat: pet model and seo schema

---
+++ back-end/features/seo/seo.model.ts
@@ -0,0 +1,11 @@
+import { SEO } from "@/shared/features/seo/domain/seo.model";
+import ImageSchema from "@/back-end/features/image/image.model";
+import { Schema } from "mongoose";
+
+const SEOSchema = new Schema<SEO>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: ImageSchema, required: true },
+}, { _id: false });
+
+export default SEOSchema;
